808f3f7017f6283e2f9962b9a1135fd4f96280fa
jack.l <i95966@126.com>
2021-01-04 00:00:10 -0800

    refactor(popup): refactor popup

--- packages/element3/src/use/popup/PopupComponent.vue
+++ packages/element3/src/use/popup/PopupComponent.vue
@@ -1,18 +1,36 @@
 <style scope>
-  .body-positon-relative {
-     position: relative;
-  }
-  .el-popup__wrapper {
-    position: fixed;
-    margin: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
+.body-positon-relative {
+  position: relative;
+}
+.el-mask {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1999;
+  width: 100%;
+  height: 100%;
+  background: black;
+}
+.el-popup__wrapper {
+  position: fixed;
+  margin: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
 </style>
 <script lang="jsx">
-import { defineComponent, reactive, computed, toRaw, Teleport, ref, getCurrentInstance, onBeforeMount } from 'vue'
+import {
+  defineComponent,
+  reactive,
+  computed,
+  toRaw,
+  Teleport,
+  ref,
+  getCurrentInstance,
+  onBeforeMount
+} from 'vue'
 import { props } from './props.ts'
 import { useZindex, useBodyStyle } from './use.ts'
 
@@ -20,27 +38,35 @@ let zIndex = 2000
 
 /**
  * 管理组件
- * 
+ *
  */
 export default defineComponent({
-    props,
-    setup() {
-       
-       const zIndex = useZindex()
-       useBodyStyle()
-       return {
-          zIndex
-       }
-    },
-    render({ $props, $attrs, $slots, zIndex }) {
-      return (
-               <Teleport to="body">
-                  <div class="el-popup__wrapper" style={{zIndex}} >
-                    {$slots.default()}
-                  </div>
-              </Teleport>
-              )
-    }
-  })
+  props,
+  setup() {
+    const zIndex = useZindex()
+    useBodyStyle()
 
+    const useMark = () => {
+      if (props.mask) {
+        // set body height
+        const bodyHeight = document.boby.clientWidth
+        return <div class="el-mask"></div>
+      }
+    }
+    return {
+      zIndex,
+      useMark
+    }
+  },
+  render({ $props, $attrs, $slots, zIndex, useMark }) {
+    return (
+      <Teleport to="body">
+        {useMark()}
+        <div class="el-popup__wrapper" style={{ zIndex }}>
+          {$slots.default()}
+        </div>
+      </Teleport>
+    )
+  }
+})
 </script>
--- packages/element3/src/use/popup/props.ts
+++ packages/element3/src/use/popup/props.ts
@@ -1,11 +1,4 @@
 export const props = {
-  show: {
-    type: Boolean,
-    default: true
-  },
-  duration: {
-    type: Number
-  },
   mask: {
     type: Boolean,
     default: false
